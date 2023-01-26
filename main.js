ee31b0b978c763e53e799eb34416ba12651c9953
harshalwasnik22 <hwasnik22@gmail.com>
2023-01-26 11:24:48 +0530

    preloader added

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // import './style.css'
+
+const fade = () => {
+    const wrapper= 
+        document.querySelector('.loader-wrapper');
+        wrapper.classList.add('fade');
+};
+window.addEventListener('load', fade);
