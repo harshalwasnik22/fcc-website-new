3b398248c69dababd1517f5776f2572f1b40eee6
harshalwasnik22 <hwasnik22@gmail.com>
2023-01-27 21:39:39 +0530

    text effect

--- main.js
+++ main.js
@@ -6,3 +6,32 @@ const fade = () => {
         wrapper.classList.add('fade');
 };
 window.addEventListener('load', fade);
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector(".text-scramble").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
